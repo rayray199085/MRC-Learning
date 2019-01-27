9a784ef2f5c8292b90c655b16b808eb19a68ad52
Rui Cao <rayray199085@gmail.com>
2019-01-27 11:39:57 +1100

    Initial Commit

---
+++ mrcLearning/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  mrcLearning
+//
+//  Created by Stephen Cao on 27/1/19.
+//  Copyright © 2019 Stephen Cao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
